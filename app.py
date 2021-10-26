4b5c37ba6a8ee641cef63e3677ada6d6c950239a
JuanMedinaMoya <the.juanmedina9@gmail.com>
2021-10-26 14:03:03 +0200

    Inicio

---
+++ app.py
@@ -0,0 +1,65 @@
+from bson.objectid import ObjectId
+from flask import Flask, json ,request ,jsonify
+from flask_pymongo import PyMongo
+from flask import render_template
+from pymongo import mongo_client
+
+
+app = Flask (__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/CarHub"
+
+mongo = PyMongo(app)
+
+db = mongo.db.Users
+
+@app.route('/create')
+def create():
+    return render_template('home/create.html')
+
+@app.route('/users' , methods = ['POST'])
+def CreateUser():
+    _name = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _password = request.form['txtPassword']
+
+    id = db.insert({
+        'name': _name,
+        'email': _correo,
+        'password': _password,
+    })
+    return render_template('home/index.html')
+    
+
+@app.route('/users' , methods = ['GET'])
+def GetUsers():
+    users = []
+    for doc in db.find():
+        users.append({
+            '_id': str(ObjectId(doc['_id'])),
+            'name': doc['name'],
+            'email': doc['email'],
+            'password': doc['password']
+        })
+
+    return render_template('/home/list.html', users=users)
+    
+@app.route('/users/<id>' , methods = ['DELETE'])
+def DeleteUser(id):
+    db.delete_one({'_id': ObjectId(id)})
+    return jsonify({'msg': 'usuario eliminado'})
+
+@app.route('/users/<id>' , methods = ['PUT'])
+def UpdateUser(id):
+    db.update_one({'_id': ObjectId(id)},{'$set':{
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'password': request.json['password']
+    }})
+    return jsonify({'msg': 'usuario actualizado'})
+@app.route('/')
+def index():
+    return render_template('home/index.html')
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
