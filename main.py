21cec025a3954768925f06f3944a9db28bea66a9
JuanMedinaMoya <the.juanmedina9@gmail.com>
2021-11-22 13:26:18 +0100

    workrrkking

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def creartrayecto():
 
     if conductor and origen and destino and horasalida and precio and numeropasajeros :
         id = Trayectos.insert(
-            {'conductor':conductor,'origen': origen, 'origen': origen, 'destino': destino, 'horasalida': horasalida, 'precio': precio, 'numeropasajeros': numeropasajeros, 'finalizado':finalizado}
+            {'conductor':conductor,'origen': origen, 'origen': origen, 'destino': destino, 'horasalida': horasalida, 'precio': precio, 'numeropasajeros': numeropasajeros, 'finalizado':finalizado, 'pasajeros': ""}
         )
         resp = jsonify("Usuario añadido")
         resp.status_code = 200
@@ -123,7 +123,7 @@ def actualizartrayecto(id):
     pasajeros = ""
     finalizado = request.json['finalizado']
 
-    Usuarios.update_one({'_id': ObjectId(id)},{'$set':{'origen': origen, 'destino': destino, 'horasalida': horasalida, 'precio': precio, 'numeropasajeros': numeropasajeros, 'finalizado': finalizado}})
+    Trayectos.update_one({'_id': ObjectId(id)},{'$set':{'origen': origen, 'destino': destino, 'horasalida': horasalida, 'precio': precio, 'numeropasajeros': numeropasajeros, 'finalizado': finalizado}})
     resp = jsonify("Trayecto actualizado")
     return resp
 
