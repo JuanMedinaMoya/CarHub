2ac28632af11a9c8de1778b8ceedb7397f5e1086
Juan Manuel Perez Gonzalez <0619885345@uma.es>
2021-12-12 01:34:44 +0100

    Cambio html extends
    Cambio utilizando JINJA2 el extends con bloques

--- main.py
+++ main.py
@@ -455,7 +455,7 @@ def buscar_usuario_nombre_apellidos(filtro):
 
 #OP CONSULTA CON RELACIONES ENTRE LAS ENTIDADES
 
-@app.route('/mis_viajes/<usuario>')
+@app.route('/mis_viajes/<usuario>', methods=['GET'])
 def mis_viajes_1(usuario):
     return redirect('/mis_viajes/' + usuario + '/' + str(1))
     
@@ -536,7 +536,7 @@ def crear_conversacion(usuario1, usuario2):
             response = jsonify({
                 'mensaje': 'Conversación creada satisfactoriamente'
             })
-            return redirect('/mis_viajes/' + usuario2) #Redirecciona a la pagina de mis viajes del pasajero que se acaba de añadir
+            return redirect('/mis_viajes/' + usuario2) # Redirecciona a la pagina de mis viajes del pasajero que se acaba de añadir
         else:
             response = jsonify({
                 'mensaje': 'Ya existía una conversación creada.'
