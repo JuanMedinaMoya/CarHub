5ebff315ab36c1eab0e0f75f32781bb9dc3e4a79
JuanMedinaMoya <the.juanmedina9@gmail.com>
2022-01-15 15:11:45 +0100

    otro try (lorenzo)

--- app.py
+++ app.py
@@ -1987,8 +1987,7 @@ def not_access_permission(error=None):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 33507))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=5000, debug=True, ssl_context="adhoc")
     
     
 ## API PAYPAL
